04473f04e4e9ade021a74dc862dbcbaf58f26ed4
hjchoirr <hjchoir@naver.com>
2024-04-15 17:17:24 +0900

    스타일링 공통, 헤더, 풋터 구성 완료

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,15 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 import { Outlet } from 'react-router-dom';
 import Header from "../outlines/Header";
 import Footer from "../outlines/Footer";
 
+const MainBox = styled.main`
+    min-height: 650px;
+`;
 const MainLayout = () => {
     return (
         <>
             <Header/>
-            <main>
+            <MainBox>
                 <Outlet />
-            </main>
+            </MainBox>
             <Footer />
         </>
     );
